e8fefdede2281c30d7859fe6da7181b6d18c06de
Sakbanullah <okta45168@gmail.com>
2025-12-09 22:46:42 +0700

    mebenerinamabot

--- server/static.ts
+++ server/static.ts
@@ -1,8 +1,13 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
 export function serveStatic(app: Express) {
+  // buat __dirname manual di Node ESM
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
   const distPath = path.resolve(__dirname, "public");
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -12,7 +17,7 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
+  // fall through ke index.html jika file tidak ada
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
